2cc7ccbffd131ac611cb2d2213a8c1a58965caae
Alex Skrashuk <askrashuk@scnsoft.com>
2022-12-14 21:22:07 +0300

    Database configuration approach changed.

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ use App\Router\Router;
 
 session_start();
 
+if (!file_exists('config.php')) {
+    die('You forget to copy <b>config.php.bak</b> to <b>config.php</b> and setup database connection credentials');
+}
 require_once 'config.php';
 require_once __DIR__ . '/vendor/autoload.php';
 
